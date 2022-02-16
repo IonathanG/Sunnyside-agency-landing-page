34ff825d4a2aa1a6f81aeaf412e1dc50a7e0c0be
Gereltodb <65011699+Gereltodb@users.noreply.github.com>
2022-02-16 02:11:14 +0000

    basics

--- index.js
+++
@@ -1,7 +0,0 @@
-const title = document.querySelector('h1');
-
-console.log(title);
-
-title.addEventListener('click', () => {
-    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank').focus();
-})
